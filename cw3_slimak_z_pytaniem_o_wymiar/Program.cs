7e64e0948cd5e718a23c20a62776f132b12a6c46
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-24 10:26:57 +0200

    1

---
+++ cw3_slimak_z_pytaniem_o_wymiar/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2_tablica_wypelniana_slimakiem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //const int Size = 10;
+            Console.WriteLine("Podaj rozmiar tablicy: ");
+            int Size = Convert.ToInt32(Console.ReadLine());
+            if (Size <= 0)
+
+            {
+                Console.WriteLine("Podałeś nieprawidłowy rozmiar tablicy, nie może być mniejszy lub równy zero.");
+            }
+            else
+            {
+                int[,] array = new int[Size, Size];
+
+                int currentCol = 0;
+                int currentRow = 0;
+                int direction = 0; // 0 - prawo; 1 - dół; 2 - lewo; 3 - góra;
+
+
+
+                for (int currentNumber = 1; currentNumber <= Size * Size; currentNumber++)
+                {
+                    array[currentRow, currentCol] = currentNumber;
+                    switch (direction)
+                    {
+                        case 0: // w prawo
+                            if (currentCol + 1 >= Size || array[currentRow, currentCol + 1] != 0)
+                            {
+                                direction = 1;
+                                currentRow++;
+                            }
+                            else
+                            {
+                                currentCol++;
+                            }
+                            break;
+                        case 1: // w dół
+                            if (currentRow + 1 >= Size || array[currentRow + 1, currentCol] != 0)
+                            {
+                                direction = 2;
+                                currentCol--;
+                            }
+                            else
+                            {
+                                currentRow++;
+                            }
+                            break;
+                        case 2: // w lewo
+                            if (currentCol - 1 < 0 || array[currentRow, currentCol - 1] != 0)
+                            {
+                                direction = 3;
+                                currentRow--;
+                            }
+                            else
+                            {
+                                currentCol--;
+                            }
+                            break;
+                        case 3: // w gore
+                            if (currentRow - 1 < 0 || array[currentRow - 1, currentCol] != 0)
+                            {
+                                direction = 0;
+                                currentCol++;
+                            }
+                            else
+                            {
+                                currentRow--;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+
+                Console.WriteLine("Tablica{0}x{0}: ", Size);
+                for (int i = 0; i < Size; i++)
+                {
+                    for (int j = 0; j < Size; j++)
+                    {
+                        Console.Write("{0,3} ", array[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ cw3_tablica_rosnaco-malejaca_z_pytaniem_o_wymiar/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw1_tablica_rosnaco_malejaca
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Podaj rozmiar tablicy: ");
+            int Size = Convert.ToInt32(Console.ReadLine());
+            if (Size <= 0)
+
+            {
+                Console.WriteLine("Podałeś nieprawidłowy rozmiar tablicy, nie może być mniejszy lub równy zero.");
+            }
+            else
+            {
+                int[,] array = new int[Size, Size];
+                int number = 1;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        for (int j = 0; j < array.GetLength(1); j++)
+                        {
+                            array[i, j] = number;
+                            number++;
+                        }
+                    }
+                    else
+                    {
+                        for (int j = array.GetLength(1) - 1; j >= 0; j--)
+                        {
+                            array[i, j] = number;
+                            number++;
+                        }
+                    }
+                }
+
+                PrintArray(array);
+            }
+        }
+
+            static void PrintArray(int[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write($"{array[i, j],4} ");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+        
+    }
+}
